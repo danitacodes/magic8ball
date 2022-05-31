2b52131d0ff723a7c433c55c9979758d81038806
Austin Calvelage <austin.calvelage@icloud.com>
2022-05-31 06:10:38 -0400

    Adjusted some CSS styles, added "shake" to eight ball

--- node-backend-simple-json/js/main.js
+++ node-backend-simple-json/js/main.js
@@ -1,11 +1,15 @@
+const eightBall =  document.querySelector('.ball');
+
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
 
   const res = await fetch(`/api/ask`)
   const data = await res.json()
-
+  eightBall.classList.add('animate__shakeY') // adds animation class
+  await new Promise(r => setTimeout(r, 2500)); // timesouts to left css animation finish
   console.log(data);
   document.querySelector("h2").innerText = data
+  eightBall.classList.remove('animate__shakeY') // removes animation class
   
 }
